787f3a15e9a151ca161052992e757ad5dbe0af02
archy <archybool@gmail.com>
2022-02-21 10:12:22 +0800

    修改

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+/*
+ * @author: Archy
+ * @Date: 2022-02-17 16:31:44
+ * @LastEditors: Archy
+ * @LastEditTime: 2022-02-21 10:06:20
+ * @FilePath: \ext-lang\index.js
+ * @description: 
+ */
 const { languages } = require("./languages");
-exports.default = (extname) => languages[extname];
+module.exports = (extname) => languages[extname];
